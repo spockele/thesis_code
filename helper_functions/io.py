d0771f7d97826e16362a5d61fd3c0c744b52bfb4
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-02-09 21:19:20 +0100

    Comments, comments and more comments for the HAWC2 read function.

--- helper_functions/io.py
+++ helper_functions/io.py
@@ -44,47 +44,70 @@ def read_hawc2_aero_noise(path: str):
     :param path: HAWC2 aeroacoustic output file
     :return:    The observer position as numpy array of floats,
                 pd.DataFrame of time series of hub position, wind speed and blade azimuths,
-                Dict with pd.DataFrame of all psd values at each timestep with time as keys
+                Dict with pd.DataFrame of all PSD values at each timestep with time as keys
     """
+    # Read the data file into python
     with open(path) as f:
         lines = f.readlines()
-
+    # Strip the lines of the newline
     lines = [line.strip('\n') for line in lines]
 
+    # Extract the observer position
     observer_pos = np.array([float(num) for num in lines[5].replace('  ', ' ').split(' ')[-3:]])
-
+    # Prepare the time series DataFrame to be filled
     time_series_data = pd.DataFrame(columns=['t', 'hub_x', 'hub_y', 'hub_z', 'hub_u', 'psi_1', 'psi_2', 'psi_3', ], )
     time_series_data.set_index('t', inplace=True)
 
+    # Create empty PSD dict
     psd = {}
+    # Create PSD DataFrame for first time step
     psd_i = pd.DataFrame(columns=['f_c', 'PSD_All', 'PSD_TI', 'PSD_TE', 'PSD_ST', 'PSD_TP',
                                   'PSD_All_1', 'PSD_TI_1', 'PSD_TE_1', 'PSD_ST_1', 'PSD_TP_1',
                                   'PSD_All_2', 'PSD_TI_2', 'PSD_TE_2', 'PSD_ST_2', 'PSD_TP_2',
                                   'PSD_All_3', 'PSD_TI_3', 'PSD_TE_3', 'PSD_ST_3', 'PSD_TP_3'])
     psd_i.set_index('f_c', inplace=True)
 
+    # Python is STOOOPID
     t = 0
+    # Indicator to start the next timestep to True
     nxt = True
+    # Loop over all the lines in the file
     for line in lines[6:]:
+        # In case of having reached a new timestep
         if nxt:
+            # Read the info line and simplify it for processing
             info = line.replace('  ', ' ').split('# ')[1:]
+            # Extract all info at this timestep in this overly complex way :)
             t = float(info[0])
             hub_pos = [float(num) for num in info[1].split(' ')[3:-1]]
             hub_vel = float(info[2].split('  ')[1])
-
             blade_azim = [limit_angle(np.radians(float(num))) for num in info[4].split(' ')[2:-1]]
 
+            # Fill this info into the DataFrame in an even better way :))
             time_series_data.loc[t] = [*hub_pos, hub_vel, *blade_azim]
+            # Set the next timestep indicator to False since we will continue to extract the PSD stuff
             nxt = False
 
+        # When an empty line is reached
         elif line == ' ':
+            # Save PSD data for the timestep
             psd[t] = psd_i
+            # We have reached the next timestep :)
             nxt = True
+            # Create PSD DataFrame for next timestep
+            psd_i = pd.DataFrame(columns=['f_c', 'PSD_All', 'PSD_TI', 'PSD_TE', 'PSD_ST', 'PSD_TP',
+                                          'PSD_All_1', 'PSD_TI_1', 'PSD_TE_1', 'PSD_ST_1', 'PSD_TP_1',
+                                          'PSD_All_2', 'PSD_TI_2', 'PSD_TE_2', 'PSD_ST_2', 'PSD_TP_2',
+                                          'PSD_All_3', 'PSD_TI_3', 'PSD_TE_3', 'PSD_ST_3', 'PSD_TP_3'])
+            psd_i.set_index('f_c', inplace=True)
 
+        # All other lines are considered PSD data (THIS MIGHT BREAK WITH ACTUAL DATA...)
         else:
+            # Split the numbers in this line from one another and fill them into the DataFrame of this timestep's PSD
             data = [float(num) for num in line.split('  ')[1:]]
             psd_i.loc[data[0]] = data[1:]
 
+    # Return all the extracted data ;|
     return observer_pos, time_series_data, psd
 
 
