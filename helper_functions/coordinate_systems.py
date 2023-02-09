7cd8154dae1f209ab36d964b8fcd945cf25d11ad
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-02-09 10:35:48 +0100

    Added a spherical coordinate system to the mix

--- helper_functions/coordinate_systems.py
+++ helper_functions/coordinate_systems.py
@@ -31,7 +31,7 @@ class Cartesian:
         elif isinstance(other, Spherical):
             return Cartesian(*(self.vec + other.to_cartesian().vec))
         else:
-            raise TypeError('Cannot add this data type with a Cartesian coordinate.')
+            raise TypeError('Cannot add this data type to a Cartesian coordinate.')
 
     def __sub__(self, other):
         if isinstance(other, type(self)):
@@ -39,15 +39,28 @@ class Cartesian:
         elif isinstance(other, Spherical):
             return Cartesian(*(self.vec - other.to_cartesian().vec))
         else:
-            raise TypeError('Cannot add this data type with a Cartesian coordinate.')
+            raise TypeError('Cannot subtract this data type from a Cartesian coordinate.')
 
     def to_spherical(self, origin):
+        """
+        Convert self to a Spherical coordinate
+        """
         x, y, z = (self - origin).vec
         r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
         theta = np.arctan2(y, x)
         phi = np.arctan2(np.sqrt(x ** 2 + y ** 2), z)
 
         return Spherical(r, theta, phi, origin)
+
+    def to_cylindrical(self, origin):
+        """
+        Convert self to a Cylindrical coordinate
+        """
+        x, y, z = (self - origin).vec
+        r = np.sqrt(x ** 2 + y ** 2)
+        psi = np.arctan2(-z, x)
+
+        return Cylindrical(r, psi, y, origin)
 
 
 class Spherical:
@@ -87,7 +100,7 @@ class Spherical:
             return cart.to_spherical(self.origin)
 
         else:
-            raise TypeError('Cannot add this data type with a Spherical coordinate.')
+            raise TypeError('Cannot add this data type to a Spherical coordinate.')
 
     def __sub__(self, other):
         """
@@ -107,15 +120,98 @@ class Spherical:
             return cart.to_spherical(self.origin)
 
         else:
-            raise TypeError('Cannot add this data type with a Spherical coordinate.')
+            raise TypeError('Cannot subtract this data type from a Spherical coordinate.')
 
     def to_cartesian(self):
+        """
+        Convert self to a Cartesian coordinate
+        """
         r, theta, phi = self.vec
         x = r * np.cos(theta) * np.sin(phi)
         y = r * np.sin(theta) * np.sin(phi)
         z = r * np.cos(phi)
 
         return Cartesian(x, y, z) + self.origin
+
+    def to_cylindrical(self, origin: Cartesian = None):
+        """
+        Convert self to a Cylindrical coordinate
+        """
+        if origin is None:
+            origin = self.origin
+
+        return self.to_cartesian().to_cylindrical(origin)
+
+
+class Cylindrical:
+    def __init__(self, r: float, psi: float, y: float, origin: Cartesian):
+        self.vec = np.array((r, limit_angle(psi), y))
+        self.origin = origin
+
+    def __str__(self):
+        return f'[{self.vec[0]}, {self.vec[1]}, {self.vec[2]}]'
+
+    def __repr__(self):
+        return f'<Cylindrical: {str(self)}, around {str(self.origin)}>'
+
+    def __add__(self, other):
+        """
+        Addition of other to self, which is then referred to self.origin.
+        """
+        if isinstance(other, type(self)) or isinstance(other, Spherical):
+            cart1 = self.to_cartesian()
+            cart2 = other.to_cartesian()
+            cart = cart1 + cart2
+
+            return cart.to_cylindrical(self.origin)
+
+        elif isinstance(other, Cartesian):
+            cart1 = self.to_cartesian()
+            cart = cart1 + other
+
+            return cart.to_cylindrical(self.origin)
+
+        else:
+            raise TypeError('Cannot add this data type to a Cylindrical coordinate.')
+
+    def __sub__(self, other):
+        """
+        Subtraction of other from self, which is then referred to self.origin.
+        """
+        if isinstance(other, type(self)) or isinstance(other, Spherical):
+            cart1 = self.to_cartesian()
+            cart2 = other.to_cartesian()
+            cart = cart1 - cart2
+
+            return cart.to_cylindrical(self.origin)
+
+        elif isinstance(other, Cartesian):
+            cart1 = self.to_cartesian()
+            cart = cart1 - other
+
+            return cart.to_cylindrical(self.origin)
+
+        else:
+            raise TypeError('Cannot subtract this data type from a Cylindrical coordinate.')
+
+    def to_cartesian(self):
+        """
+        Convert self to a Cartesian coordinate
+        """
+        r, psi, y = self.vec
+        x = r * np.cos(psi)
+        z = - r * np.sin(psi)
+
+        return Cartesian(x, y, z) + self.origin
+
+    def to_spherical(self, origin: Cartesian = None):
+        """
+        Convert self to a Spherical coordinate
+        """
+        if origin is None:
+            origin = self.origin
+
+        return self.to_cartesian().to_spherical(origin)
 
 
 if __name__ == '__main__':
