cc8bbb484808f3c6c0108f35d8ed298f72bd865c
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-02-10 11:39:26 +0100

    Switch to Python 3.10 to use librosa with FAST Griffin-Lim.

--- moving_sources.py
+++ moving_sources.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.io as spio
-import pyroomacoustics as pra
+import librosa
 import matplotlib.pyplot as plt
 
 import helper_functions.coordinate_systems as cs
@@ -18,56 +18,47 @@ class CircleMovingSource:
         psi = psi_0 + self.omega * t
         return np.array([cs.Cylindrical(r_0, p, y_0, origin) for p in psi])
 
-    def velocities_cartesian(self, t: np.array):
+    def velocities_cartesian(self, t: np.array, motion_out: bool = False):
         motion = self.motion(t)
         v = np.zeros((motion.size, 3))
         for it, tt in enumerate(t):
             pos = motion[it]
             circle_dot = self.omega * pos.vec[0]
-            v[it, 0] = -circle_dot * np.sin(pos[1])
-            v[it, 2] = -circle_dot * np.cos(pos[1])
+            v[it, 0] = -circle_dot * np.sin(pos.vec[1])
+            v[it, 2] = -circle_dot * np.cos(pos.vec[1])
 
-        return v
-
-
-def cb(epoch, _, y):
-    print(epoch)
+        return (v, motion) if motion_out else v
 
 
-def wav_and_gla_test():
+def wav_and_gla_test_librosa():
     freq, dat = spio.wavfile.read("music_samples/Queen - Bohemian Rhapsody.wav")
 
-    # These are the parameters of the STFT
-    fft_size = 512
-    hop = fft_size // 4
-    win_a = np.hamming(fft_size)
-    win_s = pra.transform.stft.compute_synthesis_window(win_a, hop)
-    n_iter = 32
+    fxx_0 = librosa.stft(dat[:, 0] / np.max(np.abs(dat)))
+    fxx_1 = librosa.stft(dat[:, 1] / np.max(np.abs(dat)))
 
-    engine = pra.transform.STFT(
-        fft_size, hop=hop, analysis_window=win_a, synthesis_window=win_s
-    )
-    fxx = engine.analysis(dat[:, 0])
-    x_mag = np.abs(fxx)
+    x_0 = librosa.griffinlim(np.abs(fxx_0))
+    x_1 = librosa.griffinlim(np.abs(fxx_1))
 
-    x_0 = pra.phase.gl.griffin_lim(x_mag, hop, win_a, fft_size, n_iter=n_iter, callback=cb, )
-
-    fxx = engine.analysis(dat[:, 1])
-    x_mag = np.abs(fxx)
-
-    x_1 = pra.phase.gl.griffin_lim(x_mag, hop, win_a, fft_size, n_iter=n_iter, callback=cb, )
+    plt.plot(dat[:, 0] / np.max(np.abs(dat)))
+    plt.plot(x_0)  # / np.max(np.abs(x_0)))
+    plt.show()
 
     new_dat = np.empty(dat.shape)
     new_dat[:, 0] = x_0
     new_dat[:, 1] = x_1
+    new_dat = new_dat / np.max(np.abs(new_dat))
 
-    new_dat = new_dat / np.max(np.abs(new_dat[np.isfinite(new_dat)]))
-    print(new_dat)
-
-    spio.wavfile.write('music_samples/Test.wav', freq, new_dat.astype('float32'))
+    spio.wavfile.write('music_samples/Test_librosa.wav', freq, new_dat.astype('float32'))
 
 
 if __name__ == '__main__':
+    # sp = cs.Cylindrical(100, 0, 0, cs.Cartesian(0, 0, -150))
+    # source = CircleMovingSource(sp, 1, )
+    #
+    # t_lst = np.linspace(0, 6.3, 631)
+    # vel_lst, pos_lst = source.velocities_cartesian(t_lst, motion_out=True)
+    #
+    # observer = cs.Cartesian(0, -500, 0)
 
-
+    wav_and_gla_test_librosa()
     # print('Hello World!')
