7af4ed70e948dc827b27ad2b64ccd4e1c9fb5fc1
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-03-29 11:47:38 +0200

    Added a head-related coordinate system to the mix

--- helper_functions/coordinate_systems.py
+++ helper_functions/coordinate_systems.py
@@ -40,6 +40,7 @@ class Cartesian:
     def to_spherical(self, origin):
         """
         Convert self to a Spherical coordinate.
+        :param origin: origin point around which to set the spherical coordinates.
         """
         x, y, z = (self - origin).vec
         r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
@@ -51,12 +52,26 @@ class Cartesian:
     def to_cylindrical(self, origin):
         """
         Convert self to a Cylindrical coordinate.
+        :param origin: origin point around which to set the cylindrical coordinates.
         """
         x, y, z = (self - origin).vec
         r = np.sqrt(x ** 2 + y ** 2)
         psi = np.arctan2(-z, x)
 
         return Cylindrical(r, psi, y, origin)
+
+    def to_hr_spherical(self, origin, rotation):
+        """
+        Convert self to a head-related spherical coordinate.
+        :param origin: origin point around which to set the spherical coordinates.
+        :param rotation: Rotation of the head in the azimuth angle. IN RADIANS.
+        """
+        x, y, z = (self - origin).vec
+        r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+        azimuth = np.arctan2(x, y) - rotation
+        polar = np.arctan2(np.sqrt(x ** 2 + y ** 2), -z)
+
+        return HeadRelatedSpherical(r, azimuth, polar, origin, rotation)
 
 
 class Spherical:
@@ -123,20 +138,35 @@ class Spherical:
         Convert self to a Cartesian coordinate.
         """
         r, theta, phi = self.vec
-        x = r * np.cos(theta) * np.sin(phi)
-        y = r * np.sin(theta) * np.sin(phi)
-        z = r * np.cos(phi)
+        x = r * np.cos(theta) * np.cos(phi)
+        y = r * np.sin(theta) * np.cos(phi)
+        z = r * np.sin(phi)
 
         return Cartesian(x, y, z) + self.origin
 
     def to_cylindrical(self, origin: Cartesian = None):
         """
         Convert self to a Cylindrical coordinate.
+        :param origin: origin point around which to set the cylindrical coordinates.  If None: around own origin point.
         """
         if origin is None:
             origin = self.origin
 
         return self.to_cartesian().to_cylindrical(origin)
+
+    def to_hr_spherical(self, origin: Cartesian = None, rotation: float = None):
+        """
+        Convert self to a head-related spherical coordinate.
+        :param origin: origin point around which to set the spherical coordinates.
+        :param rotation: Rotation of the head in the azimuth angle. IN RADIANS.
+        """
+        if origin is None:
+            origin = self.origin
+
+        if rotation is None:
+            rotation = 0.
+
+        return self.to_cartesian().to_hr_spherical(origin, rotation)
 
 
 class Cylindrical:
@@ -210,11 +240,79 @@ class Cylindrical:
     def to_spherical(self, origin: Cartesian = None):
         """
         Convert self to a Spherical coordinate.
+        :param origin: origin point around which to set the spherical coordinates. If None: around own origin point.
         """
         if origin is None:
             origin = self.origin
 
         return self.to_cartesian().to_spherical(origin)
+
+    def to_hr_spherical(self, origin: Cartesian = None, rotation: float = None):
+        """
+        Convert self to a head-related spherical coordinate.
+        :param origin: origin point around which to set the spherical coordinates.
+        :param rotation: Rotation of the head in the azimuth angle. IN RADIANS.
+        """
+        if origin is None:
+            origin = self.origin
+
+        if rotation is None:
+            rotation = 0.
+
+        return self.to_cartesian().to_hr_spherical(origin, rotation)
+
+
+class HeadRelatedSpherical:
+    def __init__(self, r, azimuth, polar, origin, rotation):
+        """
+        Class to store a point in Head-Related SPHERICAL coordinates around "origin"
+        :param r: Radial coordinate.
+        :param azimuth: Angle in xy-plane IN RADIANS.
+        :param polar: Angle in the plane; perpendicular to xy-plane, and through origin and point.
+                    Zero when parallel to z-axis. IN RADIANS.
+        :param origin: Reference point around which these coordinates are determined.
+        :param rotation: Rotation of the head in the azimuth angle. IN RADIANS.
+        """
+        self.vec = np.array((r, limit_angle(azimuth), limit_angle(polar)))
+        self.origin = origin
+        self.rotation = limit_angle(rotation)
+
+    def __str__(self):
+        return f'[{self.vec[0]}, {self.vec[1]}, {self.vec[2]}]'
+
+    def __repr__(self):
+        return f'<HR-Spherical: {str(self)}, around {str(self.origin)} with rotation {self.rotation}>'
+
+    def to_cartesian(self):
+        """
+        Convert self to a Cartesian coordinate.
+        """
+        r, az, po = self.vec
+        x = r * np.sin(az + self.rotation) * np.cos(po)
+        y = r * np.cos(az + self.rotation) * np.cos(po)
+        z = - r * np.sin(po)
+
+        return Cartesian(x, y, z) + self.origin
+
+    def to_spherical(self, origin: Cartesian = None):
+        """
+        Convert self to a Spherical coordinate.
+        :param origin: origin point around which to set the spherical coordinates. If None: around own origin point.
+        """
+        if origin is None:
+            origin = self.origin
+
+        return self.to_cartesian().to_spherical(origin)
+
+    def to_cylindrical(self, origin: Cartesian = None):
+        """
+        Convert self to a Cylindrical coordinate.
+        :param origin: origin point around which to set the cylindrical coordinates. If None: around own origin point.
+        """
+        if origin is None:
+            origin = self.origin
+
+        return self.to_cartesian().to_cylindrical(origin)
 
 
 if __name__ == '__main__':
