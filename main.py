f6fb149da852e7cef6325cb1e64a748b4856f1f3
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-03-31 14:56:22 +0200

    It's simply a mess, messier than all the rest...

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-import random
-import matplotlib.pyplot as plt
-import scipy.io as spio
-import scipy.fft as spfft
 import numpy as np
-import pandas as pd
-import librosa
 
 import helper_functions as hf
-
-
-def hawc2_sample_testing(n):
-    """
-    :param n: blade nr. (0=all, 1-3=individual blade)
-    """
-    pos, ts_data, spectrograms = hf.read_hawc2_aero_noise('hawc2_out/case10ms_noise_psd_Obs064.out')
-
-    blade_dat: pd.DataFrame = np.sqrt(spectrograms[n]['All'])
-    t_lst = np.array(blade_dat.columns)
-    delta_t = round(t_lst[1] - t_lst[0], 3)
-    f_lst = np.array(blade_dat.index)
-    n_fft = int(44100 * delta_t)
-    f_desired = np.linspace(0, 44100 // 2, n_fft // 2 + 1, )
-
-    interpolated = np.empty((f_desired.size, t_lst.size))
-    for it, ti in enumerate(t_lst):
-        orig = blade_dat.loc[:, ti]
-        interpolated[:, it] = np.interp(f_desired, f_lst, orig)
-
-    test = librosa.griffinlim(interpolated,
-                              n_iter=200, hop_length=n_fft, n_fft=n_fft, win_length=n_fft,
-                              window='boxcar', center=True, momentum=0)
-
-    test = (test / np.max(test)).astype('float32')
-
-    plt.plot(test)
-    plt.show()
-
-    spio.wavfile.write(f'test_{n}.wav', 44100, test)
 
 
 def directivity(theta, phi, mach):
@@ -59,54 +23,11 @@ def directivity(theta, phi, mach):
     return num / den
 
 
-def uniform_spherical_grid(n_points):
-    """
-    Create a uniform grid in spherical coordinates where each point represents an equal fraction of the surface area
-    of the unit sphere. Algorithm by Deserno, 2004.
-    ------------------------------------------------------------------------------------------------
-    !!! NOTE: output is not guaranteed to contain exact number of points input into the function !!!
-    ------------------------------------------------------------------------------------------------
-    :param n_points: desired number of points in the grid
-    :return: the polar and azimuth angles in numpy arrays of length n_count
-    """
-    # Area represented per point of the grid
-    point_area = 4 * np.pi / n_points
-    # Length scale associated with this area
-    dist = np.sqrt(point_area)
-    # number of polar coordinates
-    m_pol = round(np.pi / dist)
-    # Angular distance between points in polar direction
-    d_pol = np.pi / m_pol
-    # Angular distance between points in azimuth direction
-    d_azi = point_area / d_pol
-
-    # Create counter and arrays for output
-    n_count = 0
-    polar, azimuth = np.empty((2, n_points))
-    # Loop in the polar direction
-    for mi in range(m_pol):
-        # Determine polar angle
-        pol = np.pi * (mi + .5) / m_pol
-        # Determine number of azimuthal coordinates
-        m_azi = round(2 * np.pi * np.sin(pol) / d_azi)
-        # Loop over azimuth angles
-        for ni in range(m_azi):
-            # Add polar angle to output array
-            polar[n_count] = pol
-            # Determine azimuth angle and add to output array
-            azimuth[n_count] = 2 * np.pi * ni / m_azi
-            # Up the counter by 1
-            n_count += 1
-
-    # Output the output arrays, shortened to actual number of generated points
-    return polar[:n_count + 1], azimuth[:n_count + 1]
-
-
 if __name__ == '__main__':
-    # phg, thg = uniform_spherical_grid(1000)
-    # #
-    # # d = directivity(phg, thg, .9)
-    # #
+    # phg, thg = hf.uniform_spherical_grid(5000)
+    #
+    # d = directivity(phg, thg, .9)
+    #
     # y = np.sin(phg) * np.cos(thg)
     # z = np.sin(phg) * np.sin(thg)
     # x = np.cos(phg)
@@ -114,9 +35,9 @@ if __name__ == '__main__':
     # fig = plt.figure()
     # ax = fig.add_subplot(projection='3d')
     #
-    # # idx = d > 10 ** (np.log10(np.max(d)) - 5)
-    # # ax.scatter(x[idx], y[idx], z[idx], c=10 * np.log10(d[idx]), cmap='jet', )
-    # ax.scatter(x, y, z, 'k')
+    # idx = d > 20 ** (np.log10(np.max(d)) - 20)
+    # scat = ax.scatter(x[idx], y[idx], z[idx], c=20 * np.log10(d[idx]), )
+    # plt.colorbar(scat)
     #
     # ax.set_aspect('equal')
     # # Create cubic bounding box to simulate equal aspect ratio
