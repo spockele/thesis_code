f43c2678631b4aa057b8afdbb53597bc23dcf7d4
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-04-18 10:53:52 +0200

    Structured the ray stepping nicely

--- propagation_model.py
+++ propagation_model.py
@@ -10,7 +10,7 @@ The very cool propagation model of this thesis :)
 
 class SoundRay:
     def __init__(self, pos_0: hf.Cartesian, vel_0: hf.Cartesian, t_0: float, s_0: float, beam_width: float,
-                 atmosphere: hf.Atmosphere):
+                 atmosphere: hf.Atmosphere) -> None:
         """
         Class for the propagation sound ray model.
 
@@ -30,39 +30,77 @@ class SoundRay:
         self.bw = beam_width
         self.atmosphere = atmosphere
 
-    def propagate(self, delta_t, ):
+    def update_ray_velocity(self, delta_t: float) -> (hf.Cartesian, hf.Cartesian):
+        """
+        Update the ray velocity and direction forward by time step delta_t
+        :param delta_t: time step (s)
+        :return: Updated Cartesian velocity (m/s, m/s, m/s) and direction (m/s, m/s, m/s) vectors of the sound ray
+        """
         # Get height from last position
-        h = -self.pos[-1][2]
+        height = -self.pos[-1][2]
         # Determine direction change
-        dc_dh = self.atmosphere.get_speed_of_sound_gradient(h)
-        ddir_dt = self.vel[-1].len() * hf.Cartesian(0, 0, dc_dh)
-        direction: hf.Cartesian = self.dir[-1] + ddir_dt * delta_t
+        speed_of_sound_gradient = self.atmosphere.get_speed_of_sound_gradient(height)
+        direction_change: hf.Cartesian = self.vel[-1].len() * hf.Cartesian(0, 0, speed_of_sound_gradient)
+        direction: hf.Cartesian = self.dir[-1] + direction_change * delta_t
 
         # Get wind speed and speed of sound at height
-        u = self.atmosphere.get_wind_speed(h)
-        c = self.atmosphere.get_speed_of_sound(h)
+        wind_speed = self.atmosphere.get_wind_speed(height)
+        speed_of_sound = self.atmosphere.get_speed_of_sound(height)
 
         # Determine new ray velocity v = u + c * direction
-        vel: hf.Cartesian = u * hf.Cartesian(0, 1, 0) + c * direction / direction.len()
+        vel: hf.Cartesian = wind_speed * hf.Cartesian(0, 1, 0) + speed_of_sound * direction / direction.len()
+
+        # Store new velocity and direction
+        self.vel = np.append(self.vel, (vel,))
+        self.dir = np.append(self.dir, (direction,))
+
+        return vel, direction
+
+    def update_ray_position(self, delta_t: float, vel: hf.Cartesian, direction: hf.Cartesian) -> (hf.Cartesian, float):
+        """
+        Update the ray position forward by time step delta_t and determine path step delta_s
+        :param delta_t: time step (s)
+        :param vel: Cartesian velocity vector (m/s, m/s, m/s) for current time step
+        :param direction: Cartesian direction vector (m/s, m/s, m/s) for current time step
+        :return: Updated position (m, m, m) for current time step and path step (m)
+        """
         # Determine new position with forward euler stepping
-        pos_new = self.pos[-1] + vel * delta_t
+        pos_new = self.pos[-1] + self.vel[-1] * delta_t
         # Check for reflections and if so: invert z-coordinate and z-velocity and z-direction
         if pos_new[2] >= 0:
             pos_new[2] = -pos_new[2]
             vel[2] = -vel[2]
             direction[2] = -direction[2]
 
-        # Store velocity, direction and velocity
-        self.vel = np.append(self.vel, (vel,))
-        self.dir = np.append(self.dir, (direction,))
+        # Store new position
         self.pos = np.append(self.pos, (pos_new, ))
-        # Propagate time
-        self.t = np.append(self.t, self.t[-1] + delta_t)
+
         # Propagate travelled distance
-        delta_s = (vel * delta_t).len()
+        delta_s = (self.vel[-1] * delta_t).len()
         self.s = np.append(self.s, self.s[-1] + delta_s)
 
-    def pos_array(self):
+        return pos_new, delta_s
+
+    def ray_step(self, delta_t: float, ) -> (hf.Cartesian, hf.Cartesian, hf.Cartesian, float):
+        """
+        Logic for time stepping the sound rays forward one time step
+        :param delta_t: time step (s)
+        :return: Updated Cartesian velocity (m/s, m/s, m/s) and direction (m/s, m/s, m/s) vectors of the sound ray,
+                 updated sound ray position (m, m, m), and the ray path step (m)
+        """
+        vel, direction = self.update_ray_velocity(delta_t)
+        pos, delta_s = self.update_ray_position(delta_t, vel, direction)
+
+        # Propagate time
+        self.t = np.append(self.t, self.t[-1] + delta_t)
+
+        return vel, direction, pos, delta_s
+
+    def pos_array(self) -> np.array:
+        """
+        Convert awful position history to an easily readable array of shape (self.pos.size, 3)
+        :return: numpy array with full position history unpacked
+        """
         arr = np.empty((self.pos.size, 3))
         for pi, pos in enumerate(self.pos):
             arr[pi] = pos.vec
@@ -72,7 +110,8 @@ class SoundRay:
 
 if __name__ == '__main__':
     atm = hf.Atmosphere(35.5, 10.5, )
-    phi, theta = hf.uniform_spherical_grid(25)
+    phi, theta, fail = hf.uniform_spherical_grid(29)
+
     x = np.cos(theta) * np.sin(phi)
     y = np.sin(theta) * np.sin(phi)
     z = np.cos(phi)
@@ -86,12 +125,12 @@ if __name__ == '__main__':
         c_init = p_init * atm.get_speed_of_sound(0) / p_init.len()
         soundray = SoundRay(p_init + offset, c_init, 0, 0, 0, atm)
 
-        while soundray.t[-1] < 100:
-            soundray.propagate(1e-2)
+        while soundray.t[-1] < 1:
+            soundray.ray_step(1e-3)
 
-        pos = soundray.pos_array()
+        pos_arr = soundray.pos_array()
 
-        ax.plot(pos[:, 0], pos[:, 1], pos[:, 2])
+        ax.plot(pos_arr[:, 0], pos_arr[:, 1], pos_arr[:, 2])
         ax.set_xlabel('x (m)')
         ax.set_ylabel('y (m)')
         ax.set_zlabel('z (m)')
