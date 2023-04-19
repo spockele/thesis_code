96bfb94f4763ae66c5adcfce4615d66af48d4ed0
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-04-19 12:07:09 +0200

    Started the unittest folder for the main auralisation parts

---
+++ test/test_propagation_model.py
@@ -0,0 +1,12 @@
+import unittest
+
+from .. import propagation_model as pm
+from .. import helper_functions as hf
+
+
+class TestSoundRay(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
