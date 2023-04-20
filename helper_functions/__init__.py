00714f611b5c301ad42dfbe03866926216fdc233
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-04-20 14:25:06 +0200

    Added import for NonCartesian into helper functions package:

--- helper_functions/__init__.py
+++ helper_functions/__init__.py
@@ -5,7 +5,7 @@ Package with functions used in the main programme
 # Import package functions
 # ----------------------------------------------------------------------------------------------------------------------
 from .funcs import *
-from .coordinate_systems import Coordinates, Cartesian, Cylindrical, Spherical, HeadRelatedSpherical
+from .coordinate_systems import Coordinates, Cartesian, NonCartesian, Cylindrical, Spherical, HeadRelatedSpherical
 from .geometry import PerpendicularPlane3D
 from .in_out import wav_to_stft, read_from_file, read_hawc2_aero_noise, write_to_file, wav_to_stft_mono, read_ntk_data
 from .data_structures import Heap
