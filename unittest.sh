e4e8d2c8e60cf3631f514cb4d17aa6e46b3fcbfc
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-04-24 13:48:09 +0200

    Added a copy statement st the unittest reports are automatically in the thesis report on my pc

--- unittest.sh
+++ unittest.sh
@@ -1,4 +1,5 @@
 source venv/bin/activate
 coverage run -m pytest --no-header | tee ./unittest/pytest_report
 coverage report | tee ./unittest/coverage_report
+cp ./unittest/*_report ../thesis_report/source/.
 deactivate
