e468904ba977d090656d8306448898edf326b9b8
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-04-28 12:00:18 +0200

    Started on the source model

--- source_model.py
+++ source_model.py
@@ -1,54 +1,32 @@
+import os
+
 import wetb.hawc2 as h2
 import numpy as np
 
 import helper_functions as hf
 
 
-class H2SoundSource:
-    def __init__(self, turbine_radius: float, turbine_hub_height: float, path: str = None, name: str = None,
-                 filename: str = None, modelpath: str = None, jinja_tags: dict = None):
+class H2Sphere:
+    def __init__(self, h2result_path: str):
+        self.h2result_path = h2result_path
+        self.h2result_sphere = None
 
-        self.hub_height = turbine_hub_height
-        self.radius = turbine_radius
-        self.filename = filename
+    def load_sphere(self):
+        self.h2result_sphere = [os.path.join(self.h2result_path, fname)
+                                for fname in os.listdir(self.h2result_path) if fname.endswith('.out')]
 
-        if path is None:
-            self.path = '/'.join(filename.split('/')[:-1])
-        else:
-            self.path = path
+        for fname in self.h2result_sphere:
+            pos, time, psd = hf.read_hawc2_aero_noise(fname)
+            print(psd)
 
-        if name is None:
-            self.name = filename.split('/')[-1].strip('.htc')
-        else:
-            self.name = name
 
-        if jinja_tags is None:
-            jinja_tags = {}
+class SourceModel:
+    def __init__(self, aur_file_dict: dict, h2result_path: str, ):
+        self.params = aur_file_dict
+        self.h2result_path = h2result_path
 
-        self.htc = h2.HTCFile(filename, modelpath, jinja_tags)
-
-    def insert_aero_noise_sphere(self, to_file: bool = False):
-        *coordinates, fail, pd = hf.uniform_spherical_grid(255)
-        coo = np.array(coordinates)
-
-        offset = hf.Cartesian(0, 0, -self.hub_height)
-
-        pos = [hf.Cartesian(self.radius * np.cos(coo[1][idx]) * np.sin(coo[1][idx]),
-                            self.radius * np.sin(coo[1][idx]) * np.sin(coo[1][idx]),
-                            self.radius * np.cos(coo[1][idx])) + offset
-               for idx in range(coo.shape[1])]
-
-        for pi, p in enumerate(pos):
-            self.htc.aero.aero_noise.add_line(name="xyz_observer", values=p.vec, comments=f'Observer_{pi}')
-
-        if to_file:
-            self.htc.save(f'{self.path}/{self.name}.htc')
-
-    def run_simulation(self, ):
-        pass
+        self.h2sphere = H2Sphere(self.h2result_path)
 
 
 if __name__ == '__main__':
-    sound = H2SoundSource(20.5, 35.5, filename='NTK/H2model/htc/ntk_500_41.htc')
-    # sound.insert_aero_noise_sphere()
-    # print(sound.htc.aero.aero_noise)
+    pass
