a4b915be117fe932b616e3a11041ad35cff7eea3
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-05-01 13:11:21 +0200

    Small changes in the way input data is handled

--- source_model.py
+++ source_model.py
@@ -1,14 +1,12 @@
 import os
 import pandas as pd
-import wetb.hawc2 as h2
 import numpy as np
-
 import helper_functions as hf
 
 
-class Source:
-    def __init__(self, pos: hf.Cartesian, time_series, turbine_psd, blade_1_psd, blade_2_psd, blade_3_psd):
-        self.pos = pos
+class Source(hf.Cartesian):
+    def __init__(self, pos: np.array, time_series, turbine_psd, blade_1_psd, blade_2_psd, blade_3_psd):
+        super().__init__(*pos)
         self.time_series = time_series
         self.turbine_psd = turbine_psd
         self.blade_1_psd = blade_1_psd
@@ -16,21 +14,17 @@ class Source:
         self.blade_3_psd = blade_3_psd
 
     def __repr__(self):
-        return f'<Sound source at {str(self.pos)}>'
+        return f'<Sound source at {str(self)}>'
 
 
-class H2Observer:
-    def __init__(self, psd: dict, pos: hf.Cartesian, time_series: pd.DataFrame, scope: str):
-        self.turbine_psd = psd[0][scope]
-        self.blade_1_psd = psd[1][scope]
-        self.blade_2_psd = psd[2][scope]
-        self.blade_3_psd = psd[3][scope]
-
-        self.pos = pos
-        self.time_series = time_series
+class H2Observer(Source):
+    # def __init__(self, pos: np.array, time_series: pd.DataFrame, psd: dict,):
+    def __init__(self, fname: str, scope: str):
+        pos, time_series, psd = hf.read_hawc2_aero_noise(fname, scope=scope)
+        super().__init__(pos, time_series, psd[0], psd[1], psd[2], psd[3])
 
     def __repr__(self):
-        return f'<HAWC2 Observer at {str(self.pos)}>'
+        return f'<HAWC2 Observer at {str(self)}>'
 
 
 class H2Sphere(list):
@@ -43,46 +37,27 @@ class H2Sphere(list):
         out_files = [os.path.join(self.h2result_path, fname)
                      for fname in os.listdir(self.h2result_path) if fname.endswith('.out')]
 
+        p_thread = hf.ProgressThread(len(out_files), 'Loading HAWC2 results sphere')
+        p_thread.start()
         for fname in out_files:
-            pos, time_series, psd = hf.read_hawc2_aero_noise(fname)
-            self.append(H2Observer(psd, hf.Cartesian(*pos), time_series, self.scope))
+            self.append(H2Observer(fname, self.scope))
+            p_thread.update()
+
+        p_thread.stop()
 
     def interpolate_sound(self, pos: hf.Cartesian, ):
-        dist = []
-        for observer in self:
-            obs: hf.Cartesian = observer.pos
-            dist.append((pos - obs).len())
+        dist = np.array([pos.dist(observer) for observer in self])
+        sort_idx = np.argsort(dist)
+        closest = np.array(self)[sort_idx][:3]
+        closest_dist = dist[sort_idx][:3]
 
-        mx = max(dist)
-        closest = []
-        closest_dist = []
-        for i in range(4):
-            idx_min = np.argmin(dist)
-            closest.append(self[idx_min])
-            closest_dist.append(dist[idx_min])
-            dist[idx_min] = mx + 1.
+        den = sum(1 / closest_dist)
 
-        turbine_psd = 0.
-        blade_1_psd = 0.
-        blade_2_psd = 0.
-        blade_3_psd = 0.
-        time_series = 0.
-        den = 0.
-
-        for oi, observer in enumerate(closest):
-            turbine_psd += observer.turbine_psd / closest_dist[oi]
-            blade_1_psd += observer.blade_1_psd / closest_dist[oi]
-            blade_2_psd += observer.blade_2_psd / closest_dist[oi]
-            blade_3_psd += observer.blade_3_psd / closest_dist[oi]
-            time_series += observer.time_series / closest_dist[oi]
-
-            den += 1 / closest_dist[oi]
-
-        turbine_psd = turbine_psd / den
-        blade_1_psd = blade_1_psd / den
-        blade_2_psd = blade_2_psd / den
-        blade_3_psd = blade_3_psd / den
-        time_series = time_series / den
+        time_series = sum([observer.time_series / closest_dist[io] for io, observer in enumerate(closest)]) / den
+        turbine_psd = sum([observer.turbine_psd / closest_dist[io] for io, observer in enumerate(closest)]) / den
+        blade_1_psd = sum([observer.blade_1_psd / closest_dist[io] for io, observer in enumerate(closest)]) / den
+        blade_2_psd = sum([observer.blade_2_psd / closest_dist[io] for io, observer in enumerate(closest)]) / den
+        blade_3_psd = sum([observer.blade_3_psd / closest_dist[io] for io, observer in enumerate(closest)]) / den
 
         return Source(pos, time_series, turbine_psd, blade_1_psd, blade_2_psd, blade_3_psd)
 
