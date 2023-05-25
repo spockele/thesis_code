4bf7d636e53fbed4bf49249b81ed2b35cf5217a5
Josephine LINUX <j.s.pockele@student.tudelft.nl>
2023-05-25 14:33:37 +0200

    Updated unittest bash file and reports

--- unittest.sh
+++ unittest.sh
@@ -1,4 +1,4 @@
-source venv/bin/activate
+source venv_3_11_3/bin/activate
 coverage run -m pytest --no-header | tee ./unittest/pytest_report
 coverage report | tee ./unittest/coverage_report
 cp ./unittest/*_report ../thesis_report/source/.
